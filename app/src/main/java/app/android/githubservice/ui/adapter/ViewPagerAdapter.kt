0cafb7effd39ccfe1b6a361c054d1fe2f6be8d85
FaramarzAf <faramarzafzali@gmail.com>
2020-09-27 15:29:02 +0330

    bug viewpager fixed

--- app/src/main/java/app/android/githubservice/ui/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/app/android/githubservice/ui/adapter/ViewPagerAdapter.kt
@@ -3,16 +3,16 @@ package app.android.githubservice.ui.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import app.android.githubservice.ui.fragment.FollowersFragment
 import app.android.githubservice.ui.fragment.FollowingFragment
 
-class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
+class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> FollowersFragment()
-            1 -> FollowingFragment()
+            0 -> return FollowersFragment()
+            1 -> return FollowingFragment()
             else -> FollowersFragment()
         }
     }
