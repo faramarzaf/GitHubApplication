71e8a6f64a1d6b4efdb5e4488852f5f43f5c95c2
FaramarzAf <faramarzafzali@gmail.com>
2020-12-24 22:44:27 +0330

    ui login complete + login auth done

--- app/src/main/java/app/android/githubservice/ui/activity/LoginActivity.kt
+++ app/src/main/java/app/android/githubservice/ui/activity/LoginActivity.kt
@@ -10,11 +10,16 @@ import app.android.githubservice.model.network.RetrofitInstance
 import app.android.githubservice.repository.AuthRepository
 import app.android.githubservice.repository.BaseRepository
 import app.android.githubservice.repository.Resource
-import app.android.githubservice.util.*
+import app.android.githubservice.util.AVATAR_URL
+import app.android.githubservice.util.HTML_URL
+import app.android.githubservice.util.KEY_SESSION_ID
+import app.android.githubservice.util.KEY_USERNAME
 import app.android.githubservice.viewmodel.AuthViewModel
+import com.faramarzaf.sdk.af_android_sdk.core.helper.HashHelper
 import com.faramarzaf.sdk.af_android_sdk.core.helper.StringHelper
 import com.faramarzaf.sdk.af_android_sdk.core.util.MyPreferences
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : BaseActivity<AuthViewModel>(), View.OnClickListener {
 
@@ -29,27 +34,32 @@ class LoginActivity : BaseActivity<AuthViewModel>(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        val username = editTextUsername.text.toString().trim()
         showProgressBar(authProgressBar)
-        if (editTextUsername.text.toString().trim().isNotEmpty() && editTextPassword.text.toString().trim().isNotEmpty()) {
-            val username = editTextUsername.text.toString().trim()
-            val password = editTextPassword.text.toString().trim()
+        if (checkbox_remember_me.isChecked && !StringHelper.stringIsEmptyOrNull(editTextUsername.text.toString().trim())) {
             MyPreferences.writeString(this, KEY_USERNAME, username)
-            MyPreferences.writeString(this, KEY_PASSWORD, password)
+            MyPreferences.writeString(this, KEY_SESSION_ID, HashHelper.sha256(username))
             viewModel.auth(username)
-        } else if (StringHelper.stringIsEmptyOrNull(editTextUsername.text.toString().trim()) &&
-            StringHelper.stringIsEmptyOrNull(editTextPassword.text.toString().trim())
-        ) {
+            hideProgressBar(authProgressBar)
+            toActivity(MainActivity::class.java)
+            finish()
+        } else if (!checkbox_remember_me.isChecked && !StringHelper.stringIsEmptyOrNull(editTextUsername.text.toString().trim())) {
+            MyPreferences.writeString(this, KEY_USERNAME, username)
+            viewModel.auth(username)
+            hideProgressBar(authProgressBar)
+            toActivity(MainActivity::class.java)
+            finish()
+        } else if (StringHelper.stringIsEmptyOrNull(editTextUsername.text.toString().trim())) {
             hideProgressBar(authProgressBar)
             toast("Fill fields!")
         }
     }
 
     private fun checkUserIsAuth() {
-        if (MyPreferences.readBoolean(this, KEY_IS_LOGGED_IN, false)) {
-            finish()
+        if (!StringHelper.stringIsEmptyOrNull(getSessionId())) {
             toActivity(MainActivity::class.java)
-        } else
-            return
+            finish()
+        }
     }
 
     private fun handleAuthResponse() {
@@ -61,9 +71,7 @@ class LoginActivity : BaseActivity<AuthViewModel>(), View.OnClickListener {
                         toast("User not found!")
                     } else {
                         hideProgressBar(authProgressBar)
-                        MyPreferences.writeBoolean(this, KEY_IS_LOGGED_IN, true)
                         saveUsefulUrls(it.value)
-                        toActivity(MainActivity::class.java)
                     }
                 }
                 is Resource.Failure -> {
@@ -72,6 +80,7 @@ class LoginActivity : BaseActivity<AuthViewModel>(), View.OnClickListener {
                         toast("Check your connection!")
                     }
                     toast(it.toString())
+
                 }
             }
         })
--- app/src/main/java/app/android/githubservice/ui/activity/MainActivity.kt
+++ app/src/main/java/app/android/githubservice/ui/activity/MainActivity.kt
@@ -1,17 +1,12 @@
 package app.android.githubservice.ui.activity
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import app.android.githubservice.R
 import app.android.githubservice.base.BaseActivityNONVM
-import app.android.githubservice.ui.fragment.*
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivityNONVM() {
@@ -23,10 +18,13 @@ class MainActivity : BaseActivityNONVM() {
         setContentView(R.layout.activity_main)
         navController = Navigation.findNavController(this, R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        //    NavigationUI.setupActionBarWithNavController(this, navController) disabled because of no action bar theme
+        transparentToolbar(this)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, null)
     }
+
 }
