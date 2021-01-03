f5c1f121b6b0a9cd424eb9c90d8bd0816cd64611
FaramarzAf <faramarzafzali@gmail.com>
2021-01-03 13:00:57 +0330

    events pojo added

---
+++ app/src/main/java/app/android/githubservice/entity/Event/Events.kt
@@ -0,0 +1,67 @@
+package app.android.githubservice.entity.Event
+
+
+import com.google.gson.annotations.SerializedName
+
+class Events : ArrayList<Events.EventsItem>(){
+    data class EventsItem(
+        @SerializedName("actor")
+        val actor: Actor,
+        @SerializedName("created_at")
+        val createdAt: String,
+        @SerializedName("id")
+        val id: String,
+        @SerializedName("org")
+        val org: Org,
+        @SerializedName("payload")
+        val payload: Payload,
+        @SerializedName("public")
+        val `public`: Boolean,
+        @SerializedName("repo")
+        val repo: Repo,
+        @SerializedName("type")
+        val type: String
+    ) {
+        data class Actor(
+            @SerializedName("avatar_url")
+            val avatarUrl: String,
+            @SerializedName("display_login")
+            val displayLogin: String,
+            @SerializedName("gravatar_id")
+            val gravatarId: String,
+            @SerializedName("id")
+            val id: Int,
+            @SerializedName("login")
+            val login: String,
+            @SerializedName("url")
+            val url: String
+        )
+    
+        data class Org(
+            @SerializedName("avatar_url")
+            val avatarUrl: String,
+            @SerializedName("gravatar_id")
+            val gravatarId: String,
+            @SerializedName("id")
+            val id: Int,
+            @SerializedName("login")
+            val login: String,
+            @SerializedName("url")
+            val url: String
+        )
+    
+        data class Payload(
+            @SerializedName("action")
+            val action: String
+        )
+    
+        data class Repo(
+            @SerializedName("id")
+            val id: Int,
+            @SerializedName("name")
+            val name: String,
+            @SerializedName("url")
+            val url: String
+        )
+    }
+}
--- app/src/main/java/app/android/githubservice/ui/adapter/ViewPagerProfileAdapter.kt
+++ app/src/main/java/app/android/githubservice/ui/adapter/ViewPagerProfileAdapter.kt
@@ -5,27 +5,30 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import app.android.githubservice.ui.fragment.FollowersFragment
 import app.android.githubservice.ui.fragment.FollowingFragment
+import app.android.githubservice.ui.fragment.ReposFragment
 
 class ViewPagerProfileAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> return FollowersFragment()
-            1 -> return FollowingFragment()
+            0 -> return ReposFragment()
+            1 -> return FollowersFragment()
+            2 -> return FollowingFragment()
             else -> FollowersFragment()
         }
     }
 
 
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         when (position) {
-            0 -> return "Followers"
-            1 -> return "Following"
+            0 -> return "Repository"
+            1 -> return "Followers"
+            2 -> return "Following"
         }
         return super.getPageTitle(position)
     }
